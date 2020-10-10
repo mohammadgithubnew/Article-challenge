853d651bb7d84f03b0f4016358db354787851879
mohammadgithubnew <67808586+mohammadgithubnew@users.noreply.github.com>
2020-10-10 14:34:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function buttonclick()
 {
     var menuList = document.getElementById("menu");
+    var menuFollow = document.getElementById("follow");
     if (menuList.className == "menuOff")
     {
 
         menuList.className = "menuOn";
+
     } else
     {
 
         menuList.className = "menuOff";
+
     }
+
+    if (menuFollow.className == "followOff")
+    {
+
+        menuFollow.className = "followOn";
+
+    } else
+    {
+
+        menuFollow.className = "followOff";
+
+    }
+
 }
